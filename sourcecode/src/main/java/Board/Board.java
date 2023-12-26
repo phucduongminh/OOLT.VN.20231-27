8b580c9b791bc1fd592e36b484b046bc014956e0
phucduongminh <phucbopbnation@gmail.com>
2023-12-26 14:07:04 +0700

    Task 2: Player & Board

---
+++ sourcecode/src/main/java/Board/Board.java
@@ -0,0 +1,12 @@
+package Board;
+
+public class Board {
+    //Công việc
+    //Viết code phải kèm theo chú thích
+    //Yêu cầu với method print
+    public void print() {
+		//Phải in được tên của Shape và số điểm trong Shape đó
+        //VD: Nếu shape đang là Semicircle, yêu cầu in ra
+        // Semicircle : 5 point
+	}
+}
---
+++ sourcecode/src/main/java/Player/TestPlayer.java
@@ -0,0 +1,8 @@
+package Player;
+
+public class TestPlayer {
+    //Yêu cầu
+    //Sử dụng player.setPointInHand()
+    //In ra
+    //System.out.println("Player " + player.infor() + " has " + player.getTotalPoint() + " gems");
+}
