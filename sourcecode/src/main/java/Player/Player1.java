e398578ea8c58c48597c14b22beb65303a1709a6
tranphucbk <trandangphuc.hd@gmail.com>
2023-12-29 19:39:05 +0700

    make player class

--- sourcecode/src/main/java/Player/Player1.java
+++ sourcecode/src/main/java/Player/Player1.java
@@ -1,5 +1,18 @@
 package Player;
 
-public class Player1 {
-    
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class Player1 extends Player{
+    private static final List<Integer> range = Arrays.asList(1, 2, 3, 4, 5);
+    public Player1(){
+        super("Play 1");
+    }
+
+    @Override
+    public List <Integer> getRange(){
+        return  range;
+    }
+
 }
--- sourcecode/src/main/java/Player/TestPlayer.java
+++ sourcecode/src/main/java/Player/TestPlayer.java
@@ -1,8 +1,17 @@
 package Player;
 
 public class TestPlayer {
-    //Yêu cầu
-    //Sử dụng player.setPointInHand()
-    //In ra
-    //System.out.println("Player " + player.infor() + " has " + player.getTotalPoint() + " gems");
+
+    public static void main(String[] args) {
+        Player1 p1 = new Player1();
+        System.out.println(p1.getName());
+        Player2 p2 = new Player2();
+        p2.setPointInHand(20);
+        System.out.println(p2.getName() + " has " +p2.getPointInHand() + " on hand " + "and has" + p2.getTotalPoint());
+        Player p = new Player("May");
+        p.plusPoint(3);
+        System.out.println(p.getName() + " has " + p.getTotalPoint());
+
+
+    }
 }
